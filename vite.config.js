e287e7d1f18e031ce9cf562e6e25552369ffc165
jacoblurie29 <jacoblurie29@gmail.com>
2023-09-17 12:06:39 -0500

    Added some documentation

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default ({ mode }) => {
 	// Load app-level env vars to node-level env vars.
+	// https://stackoverflow.com/questions/70709987/how-to-load-environment-variables-from-env-file-using-vite
 	process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
 	return defineConfig({
