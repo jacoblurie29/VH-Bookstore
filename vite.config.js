91de3185ac2d02b864ade6e9a0554ddee69811dd
Zi Nean Teoh <zinean00@gmail.com>
2023-09-17 00:25:22 -0500

    configure vite process.env

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	define: {
+		// https://github.com/vitejs/vite/issues/1973#issuecomment-815695512
+		'process.env': process.env,
+	},
 });
