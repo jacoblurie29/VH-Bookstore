5b0d98103a16a2dfe1095e86e18feed6e05c7f19
LeonXuuu <hanliang.xu@vanderbilt.edu>
2023-09-24 18:44:25 -0500

    Added data state.

--- src/tabs/Bookshelf.jsx
+++ src/tabs/Bookshelf.jsx
@@ -1,23 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import BasicTable from './BasicTable';
 import axios from 'axios';
 
 const Bookshelf = () => {
-	let config = {
-		method: 'get',
-		maxBodyLength: Infinity,
-		url: 'http://localhost:5050/getBookSales',
-		headers: { }
-	};
 
-	axios.request(config)
-	.then((response) => {
-		let bookArray = response;
-		console.log(bookArray);
-	})
-	.catch((error) => {
-		console.log(error);
-	});
+	const [data, setData] = useState();
+
+	useEffect(()=> {
+		let config = {
+			method: 'get',
+			maxBodyLength: Infinity,
+			url: 'http://localhost:5050/getBookSales',
+			headers: { }
+		};
+	
+		axios.request(config)
+		.then((response) => {
+			setData(response);
+		})
+	
+		.catch((error) => {
+			console.log(error);
+		});
+	}, [])
+
+	console.log(data);
+	
 
 	return <div>
 		<BasicTable></BasicTable>
